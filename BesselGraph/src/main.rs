a90f58b4117226010cdb7f1af284d9205bfffc40
Rasim Labibov <postrediori@gmail.com>
2024-03-08 21:20:01 +0200

    [BesselGraph] Calculate plot points in threads

--- BesselGraph/src/main.rs
+++ BesselGraph/src/main.rs
@@ -1,11 +1,22 @@
 mod bessel_func;
 mod plot_widget;
-use plot_widget::{Area, PlotWidget, PlotFunctionInfo};
+use plot_widget::{Area, PlotWidget, PlotLines, PlotFunctionInfo};
 
 use fltk::{*, prelude::*};
 
+use std::thread;
+
 const WIDTH: i32 = 700;
 const HEIGHT: i32 = 500;
+const MARGIN: i32 = 10;
+
+const LINE_COUNT: i32 = 500;
+
+enum Message {
+    UpdateArea,
+    UpdatePlots(Vec<PlotFunctionInfo>),
+    FinishCalculation((PlotFunctionInfo, PlotLines)),
+}
 
 fn main() {
     // Plot parameters
@@ -33,91 +44,279 @@ fn main() {
         .with_label("Bessel Function");
     wind.make_resizable(true);
 
-    // Plot widget
-    const PLOT_WIDGET_SIZE: i32 = HEIGHT - 20;
-    let mut plot = PlotWidget::new(WIDTH - PLOT_WIDGET_SIZE - 10, 10, PLOT_WIDGET_SIZE, PLOT_WIDGET_SIZE);
-    plot.set_area(DEFAULT_AREA);
+    let mut main_layout = group::Flex::default_fill().row();
+    main_layout.set_margin(MARGIN);
+    main_layout.set_spacing(MARGIN);
 
-    for p in &plots {
-        plot.add_plot(p);
+    // Instruments column
+    let mut controls_column = group::Flex::default_fill().column();
+
+    {
+        let spacer = frame::Frame::default();
+        controls_column.fixed(&spacer, 5);
     }
-
 
     // Plot bounds
     let mut bounds_frame = group::Group::default()
-        .with_size(WIDTH - PLOT_WIDGET_SIZE - 30, 230)
-        .with_pos(10, 20)
+        .with_size(100, 100)
+        .with_pos(10, 0)
         .with_label("Plot bounds");
     bounds_frame.set_frame(enums::FrameType::BorderFrame);
     bounds_frame.set_color(enums::Color::Black);
 
-    let mut in_max_x = input::FloatInput::default()
-        .with_size(90, 25).with_pos(bounds_frame.x()+65, bounds_frame.y()+10)
-        .with_label("x max = ");
-    let xmax_str = format!("{:.1}", DEFAULT_AREA.xmax);
-    in_max_x.set_value(&xmax_str);
+    let mut bounds_frame_flex = group::Flex::default_fill().column();
+    bounds_frame_flex.set_margin(MARGIN);
 
-    let mut in_min_x = input::FloatInput::default()
-        .with_size(90, 25).below_of(&in_max_x, 10)
-        .with_label("x min = ");
-    let xmin_str = format!("{:.1}", DEFAULT_AREA.xmin);
-    in_min_x.set_value(&xmin_str);
+    {
+        let spacer = frame::Frame::default();
+        bounds_frame_flex.fixed(&spacer, 5);
+    }
 
-    let mut in_max_y = input::FloatInput::default()
-        .with_size(90, 25).below_of(&in_min_x, 10)
-        .with_label("y max = ");
-    let ymax_str = format!("{:.1}", DEFAULT_AREA.ymax);
-    in_max_y.set_value(&ymax_str);
+    let mut in_max_y;
+    {
+        let mut row = group::Flex::default_fill().row();
 
-    let mut in_min_y = input::FloatInput::default()
-        .with_size(90, 25).below_of(&in_max_y, 10)
-        .with_label("y min = ");
-    let ymin_str = format!("{:.1}", DEFAULT_AREA.ymin);
-    in_min_y.set_value(&ymin_str);
+        frame::Frame::default();
+        
+        in_max_y = input::FloatInput::default()
+            .with_label("y max");
+        in_max_y.set_align(enums::Align::Top);
+        row.fixed(&in_max_y, 75);
 
-    let mut btn_redraw = button::Button::default()
-        .with_size(90, 25).below_of(&in_min_y, 10)
-        .with_label("Update");
+        frame::Frame::default();
 
-    btn_redraw.set_callback(move |_b| {
-        let max_x: f64 = in_max_x.value().parse::<f64>().expect("Not a number!");
-        let min_x: f64 = in_min_x.value().parse::<f64>().expect("Not a number!");
-        let max_y: f64 = in_max_y.value().parse::<f64>().expect("Not a number!");
-        let min_y: f64 = in_min_y.value().parse::<f64>().expect("Not a number!");
+        row.end();
 
-        plot.set_area(plot_widget::Area{xmin: min_x, xmax: max_x, ymin: min_y, ymax: max_y});
-        plot.redraw();
-    });
+        bounds_frame_flex.fixed(&row, 25);
+    }
+
+    let mut in_min_x;
+    let mut in_max_x;
+    {
+        let mut row = group::Flex::default_fill().row();
+
+        frame::Frame::default();
+        
+        in_min_x = input::FloatInput::default()
+            .with_label("x min");
+        in_min_x.set_align(enums::Align::Left);
+        row.fixed(&in_min_x, 45);
+
+        in_max_x = input::FloatInput::default()
+            .with_label("x max");
+        in_max_x.set_align(enums::Align::Right);
+        row.fixed(&in_max_x, 45);
+
+        frame::Frame::default();
+
+        row.end();
+
+        bounds_frame_flex.fixed(&row, 25);
+    }
+
+    let mut in_min_y;
+    {
+        let mut row = group::Flex::default_fill().row();
+
+        frame::Frame::default();
+        
+        in_min_y = input::FloatInput::default()
+            .with_label("y min");
+        in_min_y.set_align(enums::Align::Bottom);
+        row.fixed(&in_min_y, 75);
+
+        frame::Frame::default();
+
+        row.end();
+
+        bounds_frame_flex.fixed(&row, 25);
+    }
+
+    {
+        let spacer = frame::Frame::default();
+        bounds_frame_flex.fixed(&spacer, 15);
+    }
+
+    let mut btn_redraw;
+    {
+        let mut row = group::Flex::default_fill().row();
+
+        frame::Frame::default();
+        
+        btn_redraw = button::Button::default()
+            .with_label("Update");
+        row.fixed(&btn_redraw, 90);
+
+        frame::Frame::default();
+
+        row.end();
+
+        bounds_frame_flex.fixed(&row, 25);
+    }
+
+    bounds_frame_flex.end();
 
     bounds_frame.end();
+    // controls_column.fixed(&bounds_frame, 230);
 
+    {
+        let spacer = frame::Frame::default();
+        controls_column.fixed(&spacer, 10);
+    }
 
     // Legend
     let mut legend_frame = group::Group::default()
-        .with_size(bounds_frame.w(), 200).below_of(&bounds_frame, 30)
+        .with_pos(0, 0)
+        .with_size(100, 100)
         .with_label("Legend");
     legend_frame.set_frame(enums::FrameType::BorderFrame);
     legend_frame.set_color(enums::Color::Black);
 
-    let mut pack = group::Pack::default()
-        .with_size(legend_frame.w()-20, legend_frame.h()-20)
-        .with_pos(legend_frame.x()+10, legend_frame.y()+10);
-    pack.set_spacing(10);
+    let mut pack = group::Flex::default().column()
+        .with_pos(legend_frame.x(), legend_frame.y())
+        .with_size(legend_frame.w(), legend_frame.h());
+    pack.set_spacing(MARGIN);
+    pack.set_margin(MARGIN);
 
-    for p in &plots {
-        let mut legend = frame::Frame::default()
-            .with_size(90, 30)
-            .with_label(&p.name);
-        legend.set_frame(enums::FrameType::FlatBox);
-        legend.set_color(enums::Color::lighter(&p.color));
-    }
+    // controls_column.fixed(&legend_frame, 200);
 
     pack.end();
 
     legend_frame.end();
 
+    controls_column.end();
+    main_layout.fixed(&controls_column, 190);
+
+    // Plot widget column
+    let mut plot_widget = PlotWidget::new(0, 0, 100, 100);
+
+    main_layout.end();
+    
     wind.end();
+
+    let (tx, rx) = app::channel::<Message>();
+    
+    // Callbacks
+    in_max_x.set_callback({
+        let tx = tx.clone();
+        move |_b| {
+           tx.send(Message::UpdateArea);
+        }
+    });
+    in_min_x.set_callback({
+        let tx = tx.clone();
+        move |_b| {
+           tx.send(Message::UpdateArea);
+        }
+    });
+    in_max_y.set_callback({
+        let tx = tx.clone();
+        move |_b| {
+           tx.send(Message::UpdateArea);
+        }
+    });
+    in_min_y.set_callback({
+        let tx = tx.clone();
+        move |_b| {
+           tx.send(Message::UpdateArea);
+        }
+    });
+
+    btn_redraw.set_callback({
+        let tx = tx.clone();
+        move |_b| {
+           tx.send(Message::UpdateArea);
+        }
+    });
+
+    // Initial setup
+    {
+        let area = DEFAULT_AREA;
+
+        plot_widget.set_area(area);
+        
+        in_max_x.set_value(format!("{:.1}", area.xmax).as_str());
+        in_min_x.set_value(format!("{:.1}", area.xmin).as_str());
+        in_max_y.set_value(format!("{:.1}", area.ymax).as_str());
+        in_min_y.set_value(format!("{:.1}", area.ymin).as_str());
+    }
+
+    // Setup plot legend
+    let mut legend: Vec<frame::Frame> = vec![];
+    {
+        pack.begin();
+        for p in &plots {
+            let mut row = group::Flex::default().row();
+
+            let mut legend_color = frame::Frame::default();
+            legend_color.set_frame(enums::FrameType::FlatBox);
+            legend_color.set_color(enums::Color::lighter(&p.color));
+            row.fixed(&legend_color, 45);
+
+            let mut legend_name = frame::Frame::default()
+                .with_label(&p.name);
+            legend_name.set_label_color(enums::Color::Dark3);
+            legend.push(legend_name);
+
+            row.end();
+            pack.fixed(&row, 30);
+        }
+        pack.end();
+    }
+
     wind.show();
 
-    a.run().unwrap();
+    // Calculate functions
+    tx.send(Message::UpdatePlots(plots.clone()));
+
+    while a.wait() {
+        if let Some(msg) = rx.recv() {
+            match msg {
+                Message::UpdateArea => {
+                    let max_x: f64 = in_max_x.value().parse::<f64>().expect("Not a number!");
+                    let min_x: f64 = in_min_x.value().parse::<f64>().expect("Not a number!");
+                    let max_y: f64 = in_max_y.value().parse::<f64>().expect("Not a number!");
+                    let min_y: f64 = in_min_y.value().parse::<f64>().expect("Not a number!");
+        
+                    plot_widget.set_area(plot_widget::Area{xmin: min_x.max(0.0), xmax: max_x, ymin: min_y, ymax: max_y});
+        
+                    tx.send(Message::UpdatePlots(plots.clone()));
+                },
+                Message::UpdatePlots(plots) => {
+                    plot_widget.clear_plots();
+                    plot_widget.redraw();
+
+                    for w in &mut legend {
+                        w.set_label_color(enums::Color::Dark3);
+                        w.redraw_label();
+                    }
+
+                    let area = plot_widget.get_area().clone();
+                    let _ = plots.into_iter().map(|p| {
+                        thread::spawn({
+                            let tx = tx.clone();
+                            move || {
+                                println!("Start calculation of plot {}", p.name);
+                                
+                                // Calculate plot points
+                                let lines_vec = p.calc_points(LINE_COUNT, &area);
+                                tx.send(Message::FinishCalculation((p, lines_vec)));
+                            }
+                        });
+                    }).collect::<Vec<_>>();
+                },
+                Message::FinishCalculation(p) => {
+                    println!("Calculated plot {} with {} lines", &p.0.name, p.1.len());
+
+                    if let Some(w) = legend.iter_mut().find(|w| { w.label() == p.0.name }) {
+                        w.set_label_color(p.0.color.darker());
+                        w.redraw_label();
+                    }
+
+                    plot_widget.add_plot(p);
+                    plot_widget.redraw();
+                }
+            }
+        }
+    }
 }
--- BesselGraph/src/plot_widget.rs
+++ BesselGraph/src/plot_widget.rs
@@ -14,12 +14,10 @@ pub struct Point {
     y: f64,
 }
 
-pub type PlotPoints = Vec<Point>;
+pub type Line = (Point, Point);
 
-struct Tick {
-    begin: Point,
-    end: Point,
-}
+// pub type PlotPoints = Vec<Point>;
+pub type PlotLines = Vec<Line>;
 
 #[derive(Copy, Clone)]
 pub struct Area {
@@ -27,6 +25,12 @@ pub struct Area {
     pub ymin: f64,
     pub xmax: f64,
     pub ymax: f64,
+}
+
+impl Area {
+    fn is_inside(&self, p: &Point) -> bool {
+        p.x>self.xmin && p.x<self.xmax && p.y>self.ymin && p.y<self.ymax
+    }
 }
 
 type PlotFunction = fn(f64) -> f64;
@@ -40,22 +44,20 @@ pub struct PlotFunctionInfo {
 }
 
 impl PlotFunctionInfo {
-    pub fn calc_points(&self, point_count: i32, area: &Area) -> PlotPoints {
-        let mut points: PlotPoints = Vec::new();
-        points.reserve((point_count + 1) as usize);
-        let px: f64 = (area.xmax - area.xmin) / (point_count as f64);
-        for i in 0..point_count+1 {
-            let xc: f64 = area.xmin + px * (i as f64);
-            points.push(Point{ x: xc, y: (self.f)(xc) });
-        }
-        points
+    pub fn calc_points(&self, line_count: i32, area: &Area) -> PlotLines {
+        let px: f64 = (area.xmax - area.xmin) / (line_count as f64);
+        (0..line_count).map(|i| {
+            let x1: f64 = area.xmin + px * (i as f64);
+            let x2: f64 = area.xmin + px * ((i + 1) as f64);
+            (Point{ x: x1, y: (self.f)(x1) }, Point{ x: x2, y: (self.f)(x2) })
+        }).collect::<PlotLines>()
     }
 }
 
 pub struct PlotWidget {
     inner: widget::Widget,
     area: Rc<RefCell<Area>>,
-    plots: Rc<RefCell<Vec<PlotFunctionInfo>>>
+    plots: Rc<RefCell<Vec<(PlotFunctionInfo, PlotLines)>>>
 }
 
 impl PlotWidget {
@@ -65,17 +67,17 @@ impl PlotWidget {
         let area = Area{ xmin: -1.0, xmax: 1.0, ymin: -1.0, ymax: 1.0 };
         let area = Rc::from(RefCell::from(area));
 
-        let plots: Vec<PlotFunctionInfo> = Vec::new();
+        let plots: Vec<(PlotFunctionInfo, PlotLines)> = Vec::new();
         let plots = Rc::from(RefCell::from(plots));
 
         const MARGIN: i32 = 20;
         const TICK_SIZE: i32 = 10;
 
-        let point_count: i32 = 500;
         let tick_count: i32 = 20;
 
+        const BOUNDS_COLOR: enums::Color = enums::Color::Dark3;
         const BG_COLOR: enums::Color = enums::Color::White;
-        const BOUNDS_COLOR: enums::Color = enums::Color::Black;
+        const PLOT_BOUNDS_COLOR: enums::Color = enums::Color::Black;
         const TICKS_COLOR: enums::Color = enums::Color::Black;
         const AXES_COLOR: enums::Color = enums::Color::Black;
 
@@ -87,38 +89,40 @@ impl PlotWidget {
             let plots = plots_val.borrow();
 
             // Calculate pixel scale
-            let scale_x: f64 = (area.xmax - area.xmin) / ((i.w() - MARGIN * 2 - TICK_SIZE) as f64);
+            let scale_x: f64 = (area.xmax - area.xmin) / ((i.w() - MARGIN * 3 - TICK_SIZE) as f64);
             let scale_y: f64 = (area.ymax - area.ymin) / ((i.h() - MARGIN * 2 - TICK_SIZE) as f64);
 
             // Helper functions
-            let get_x = |x: f64| -> i32 { ((x - area.xmin) / scale_x) as i32 + i.x() + MARGIN + TICK_SIZE };
+            let get_x = |x: f64| -> i32 { ((x - area.xmin) / scale_x) as i32 + i.x() + MARGIN * 2 + TICK_SIZE };
             let get_y = |y: f64| -> i32 { ((area.ymax - y) / scale_y) as i32 + i.y() + MARGIN };
             let get_coord = |x: f64, y:f64| -> draw::Coord<i32> { draw::Coord::<i32>(get_x(x), get_y(y)) };
 
             // Calculate ticks
 
             // X-Axis ticks
-            let mut ticks_x: Vec<Tick> = Vec::new();
+            let mut ticks_x: Vec<Line> = Vec::new();
+            ticks_x.reserve((tick_count + 1) as usize);
             let tx: f64 = (area.xmax - area.xmin) / (tick_count as f64);
             for i in 0..tick_count+1 {
                 let xc: f64 = area.xmin + tx * (i as f64);
                 let tick_scale: f64 = if i % 2 == 0 { 1.0 } else { 0.5 };
-                ticks_x.push(Tick {
-                    begin: Point{ x: xc, y: area.ymin },
-                    end: Point{ x: xc, y: area.ymin - (TICK_SIZE as f64) * scale_y * tick_scale }
-                } );
+                ticks_x.push( (
+                    Point{ x: xc, y: area.ymin },
+                    Point{ x: xc, y: area.ymin - (TICK_SIZE as f64) * scale_y * tick_scale }
+                ) );
             }
 
             // Y-Axis ticks
-            let mut ticks_y: Vec<Tick> = Vec::new();
+            let mut ticks_y: Vec<Line> = Vec::new();
+            ticks_y.reserve((tick_count + 1) as usize);
             let ty: f64 = (area.ymax - area.ymin) / (tick_count as f64);
             for i in 0..tick_count+1 {
                 let yc: f64 = area.ymin + ty * (i as f64);
                 let tick_scale: f64 = if i % 2 == 0 { 1.0 } else { 0.5 };
-                ticks_y.push(Tick {
-                    begin: Point{ x: area.xmin, y: yc },
-                    end: Point{ x: area.xmin - (TICK_SIZE as f64) * scale_x * tick_scale , y: yc }
-                } );
+                ticks_y.push( (
+                    Point{ x: area.xmin, y: yc },
+                    Point{ x: area.xmin - (TICK_SIZE as f64) * scale_x * tick_scale , y: yc }
+                ) );
             }
 
             // Set default style for ranges and ticks
@@ -127,8 +131,12 @@ impl PlotWidget {
             // Clean draw area with backgorund color
             draw::draw_rect_fill(i.x(), i.y(), i.w(), i.h(), BG_COLOR);
 
-            // Draw bounding box
+            // Widget bounds
             draw::set_draw_color(BOUNDS_COLOR);
+            draw::draw_rect(i.x(), i.y(), i.w(), i.h());
+            
+            // Draw bounding box
+            draw::set_draw_color(PLOT_BOUNDS_COLOR);
             draw::draw_loop3(
                 get_coord(area.xmin, area.ymin),
                 get_coord(area.xmax, area.ymin),
@@ -160,62 +168,55 @@ impl PlotWidget {
 
             for t in &ticks_x {
                 draw::draw_line(
-                    get_x(t.begin.x), get_y(t.begin.y),
-                    get_x(t.end.x), get_y(t.end.y));
+                    get_x(t.0.x), get_y(t.0.y),
+                    get_x(t.1.x), get_y(t.1.y));
             }
 
             for t in &ticks_y {
                 draw::draw_line(
-                    get_x(t.begin.x), get_y(t.begin.y),
-                    get_x(t.end.x), get_y(t.end.y));
+                    get_x(t.0.x), get_y(t.0.y),
+                    get_x(t.1.x), get_y(t.1.y));
             }
 
             // Draw ranges
-            draw::set_draw_color(BOUNDS_COLOR);
+            draw::set_draw_color(PLOT_BOUNDS_COLOR);
             draw::set_font(enums::Font::Helvetica, 12);
 
-            let xmin_str = format!("{:.1}", area.xmin);
-            draw::draw_text2(&xmin_str,
+            draw::draw_text2(format!("{:.2}", area.xmin).as_str(),
                 get_x(area.xmin),
                 get_y(area.ymin - scale_y * (TICK_SIZE as f64 * 1.25)),
                 0, 0, enums::Align::TopLeft);
 
-            let xmax_str = format!("{:.1}", area.xmax);
-            draw::draw_text2(&xmax_str,
+            draw::draw_text2(format!("{:.2}", area.xmax).as_str(),
                 get_x(area.xmax),
                 get_y(area.ymin - scale_y * (TICK_SIZE as f64 * 1.25)),
                 0, 0, enums::Align::TopRight);
 
-            let ymin_str = format!("{:.1}", area.ymin);
-            draw::draw_text2(&ymin_str,
+            draw::draw_text2(format!("{:.2}", area.ymin).as_str(),
                 get_x(area.xmin - scale_x * (TICK_SIZE as f64 * 1.25)),
                 get_y(area.ymin),
                 0, 0, enums::Align::BottomRight);
 
-            let ymax_str = format!("{:.1}", area.ymax);
-            draw::draw_text2(&ymax_str,
+            draw::draw_text2(format!("{:.2}", area.ymax).as_str(),
                 get_x(area.xmin - scale_x * (TICK_SIZE as f64 * 1.25)),
                 get_y(area.ymax),
                 0, 0, enums::Align::TopRight);
 
             // Draw plots
-            let mut width: i32 = (plots.len() * 3 - 1) as i32; // Width of the lower-most plot
-            for p in plots.iter() {
-                draw::set_draw_color(p.color);
-                draw::set_line_style(draw::LineStyle::Solid, width);
-                draw::begin_line();
+            if !plots.is_empty() {
+                let mut width: i32 = (plots.len() * 3 - 1) as i32; // Width of the lower-most plot
+                for p in plots.iter() {
+                    draw::set_draw_color(p.0.color);
+                    draw::set_line_style(draw::LineStyle::Solid | draw::LineStyle::CapRound, width);
 
-                // Calculate plot points
-                let point_vec = p.calc_points(point_count, &area);
-                for c in &point_vec {
-                    if c.x>area.xmin && c.x<area.xmax && c.y>area.ymin && c.y<area.ymax {
-                        draw::vertex(get_x(c.x) as f64, get_y(c.y) as f64);
+                    for l in &p.1 {
+                        if area.is_inside(&l.0) && area.is_inside(&l.1) {
+                            draw::draw_line(get_x(l.0.x), get_y(l.0.y), get_x(l.1.x), get_y(l.1.y));
+                        }
                     }
+
+                    width -= 3; // Decrease width of plots lines as it goes to the top
                 }
-
-                width -= 3; // Decrease width of plots lines as it goes to the top
-
-                draw::end_line();
             }
             
             draw::set_line_style(draw::LineStyle::Solid, 0);
@@ -236,8 +237,20 @@ impl PlotWidget {
         *self.area.borrow_mut() = new_area;
     }
 
-    pub fn add_plot(&mut self, new_plot: &PlotFunctionInfo) {
-        self.plots.borrow_mut().push(new_plot.clone());
+    pub fn get_area(&self) -> Area {
+        *self.area.borrow()
+    }
+
+    pub fn clear_plots(&mut self) {
+        self.plots.borrow_mut().clear();
+    }
+
+    pub fn add_plot(&mut self, new_plot: (PlotFunctionInfo, PlotLines)) {
+        self.plots.borrow_mut().push((new_plot.0.clone(), new_plot.1));
+    }
+
+    pub fn redraw(&mut self) {
+        self.inner.redraw();
     }
 
 }
